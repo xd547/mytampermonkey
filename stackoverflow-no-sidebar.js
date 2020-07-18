f8b1a53e3822a22a1688e1d08493c3c5c02fe660
RainySummer <rainysummer@gmail.com>
2020-07-18 15:19:53 +0800

    Update stackoverflow-no-sidebar.js

--- stackoverflow-no-sidebar.js
+++ stackoverflow-no-sidebar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stackoverflow no sidebar
 // @namespace    http://blog.xd547.com/
-// @version      0.1
+// @version      0.2
 // @description  Hide stackoverflow left sidebar
 // @author       RainySummer
 // @match        https://stackoverflow.com/questions/*
