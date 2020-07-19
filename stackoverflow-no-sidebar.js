5a0d808e9fb9ad32e0bf84dd79e4f7cb7b7e2f27
RainySummer <rainysummer@gmail.com>
2020-07-19 15:08:41 +0800

    Add new macth to stackoverflow-no-sidebar.js

--- stackoverflow-no-sidebar.js
+++ stackoverflow-no-sidebar.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Stackoverflow no sidebar
 // @namespace    https://blog.xd547.com/
-// @version      0.4
+// @version      0.4.1
 // @description  Hide stackoverflow left sidebar
 // @author       RainySummer
 // @match        https://stackoverflow.com/questions/*
 // @match        https://*.stackexchange.com/questions/*
+// @match        https://serverfault.com/questions/*
 // @grant        none
 // ==/UserScript==
 
