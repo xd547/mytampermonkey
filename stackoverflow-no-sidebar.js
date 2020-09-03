4c2bb00a8caf69ea2d435c9c788157f141d32e37
RainySummer <rainysummer@gmail.com>
2020-09-03 17:30:29 +0800

    Add new match superuser to stackoverflow-no-sidebar.js

--- stackoverflow-no-sidebar.js
+++ stackoverflow-no-sidebar.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Stackoverflow no sidebar
 // @namespace    https://blog.xd547.com/
-// @version      0.4.2
+// @version      0.4.3
 // @description  Hide stackoverflow left sidebar
 // @author       RainySummer
 // @match        https://stackoverflow.com/questions/*
 // @match        https://*.stackexchange.com/questions/*
 // @match        https://serverfault.com/questions/*
 // @match        https://askubuntu.com/questions/*
+// @match        https://superuser.com/questions/*
 // @grant        none
 // ==/UserScript==
 
